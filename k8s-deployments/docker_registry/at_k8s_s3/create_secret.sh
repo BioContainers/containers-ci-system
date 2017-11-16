db7e6e823a3b3e71e1e726186fd31bda6ec97b72
Pablo Moreno <pablo.a.moreno@gmail.com>
2017-11-16 17:14:01 +0000

    docker registry with s3 on k8s

---
+++ k8s-deployments/docker_registry/at_k8s_s3/create_secret.sh
@@ -0,0 +1,2 @@
+kubectl create secret generic phenomenal-s3-ebi-registry-access --from-file=config.yml
+
---
+++ k8s-deployments/docker_registry/at_k8s_s3_read/create_secret.sh
@@ -0,0 +1,2 @@
+kubectl create secret generic phenomenal-s3-ebi-registry-access-read --from-file=config.yml
+
